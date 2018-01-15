d6d6f24627f540228840c2b46727c43a3a45afeb
SearcherLA <searcherla@ymail.com>
2018-01-15 13:23:12 +0100

    Anpassungen Layout / devise-pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     if user_signed_in?
-      redirect_to :controller=>'team', :action => 'index'
+      redirect_to :controller=>'teams', :action => 'index'
     end
   end
 
